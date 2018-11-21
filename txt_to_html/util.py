1ee1da055c365519bbbb98ced85762c917043a88
suprecm7 <brownchen7s@gmail.com>
2018-11-21 22:00:39 +0800

    利用生成器将文本内容划分成一段段的文本块

--- txt_to_html/util.py
+++ txt_to_html/util.py
@@ -1,16 +1,16 @@
-def lines(file):                #收集文本文件除空行外的所有行，并在文件结束添加空行
-    for line in file: yield line            #生成器中每yield一个值就会暂停等待下一次调用生成器对象
-    yield '\n'
+def lines(file):                                #一行行读取文件（包括空行）并在文件结束添加一个空行
+    for line in file: yield line             #生成器中每yield一个值就会暂停等待下一次调用生成器对象
+    yield '\n'                                   #读取最后一行就加一个空行
 
-def blocks(file):
+def blocks(file):                             #block生成器功能：依次输出一个文本块
     block = []
-    for line in lines(file):
-        if line.strip():
-            block.append(line)
-        elif block:
-            yield ' '.join(block).strip()
+    for line in lines(file):                  #依次取出一行内容
+        if line.strip():                         #判断是否为空行
+            block.append(line)           #若不是，则会将那一文本块加入block列表
+        elif block:                             #若是，则表示已经读完一整段，将其输出
+            yield ' '.join(block).strip()   #一段话的不同行需要添加空格分隔开，因为之前都strip过
             block = []
 
 
 
-#应加强对生成器的理解
+
